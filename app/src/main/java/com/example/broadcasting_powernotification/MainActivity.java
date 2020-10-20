c29ee7610f482cb7c825113db8fe01c625fd4ff4
SAYAN490 <sayanbanerjee.490@gmail.com>
2020-10-20 21:39:34 +0530

    Initial commit.

---
+++ app/src/main/java/com/example/broadcasting_powernotification/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.broadcasting_powernotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver=new CustomReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        IntentFilter filter=new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+
+        this.registerReceiver(mReceiver,filter);
+    }
+    protected void onDestroy()
+    {
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+}
